81a345869f39bdd80b43dba5ab953446858fec74
franck <franck@sylvain-girod.com@srvtl-hostedgre-6v01frgr-up07.sylvain-girod.com>
2020-08-17 15:00:59 +0200

    enforce TLSv1.3

--- main.go
+++ main.go
@@ -243,14 +243,10 @@ func setupTLS() (*tls.Config, error) {
 
 	tlsConfig = &tls.Config{
 		Certificates: []tls.Certificate{cert},
-		MinVersion:   tls.VersionTLS12,
+		MinVersion:   tls.VersionTLS13,
 		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+                        tls.TLS_CHACHA20_POLY1305_SHA256,
 		},
 	}
 	tlsConfig.BuildNameToCertificate()
